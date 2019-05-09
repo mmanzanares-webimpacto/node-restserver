bc4e2a8b4cf5fdafac42b37412c52af6369dabd9
mmanzanareswebimpacto <50134749+mmanzanareswebimpacto@users.noreply.github.com>
2019-05-09 16:46:07 +0200

    Add files via upload

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,40 @@
 // ================================
 //             PUERTO
 // ================================
-
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ================================
+//             ENTORNO
+// ================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ================================
+//      VENCIMIENTO DEL TOKEN
+// ================================
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 días
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+// ================================
+//       SEED DE AUTENTICACIÓN
+// ================================
+process.env.SEED = process.env.SEED || 'secret';
+
+
+// ================================
+//             BBDD
+// ================================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://mmanzanares:Manzanares98*@cluster0-gziva.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -8,34 +10,20 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 
+//Configuración global de rutas
+app.use(require('./routes/index'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
 
-    res.json({
-        body
+        if (err) throw err;
+
+        console.log('Base de datos ONLINE!');
     });
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
-
 
 
 app.listen(process.env.PORT, () => {
